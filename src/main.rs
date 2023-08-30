a9af0679962e770ceca3e93a8e7eab948b5b8735
Jacob Sinclair <jcbsnclr@outlook.com>
2023-08-30 21:25:25 +0100

    define preliminary grammar for language, give test input

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,19 @@ mod parser;
 use parser::Rule;
 use pest::Parser;
 
-fn main() {}
+const SRC: &'static str = r#"
+def a "hello, world"
+def a 123
+def a hello_world123asd
+def a < a = b > 
+def a ( a )
+def a [ a ]
+def a 1
+symbol test
+"#;
+
+fn main() {
+    let result = parser::RivetParser::parse(Rule::program, SRC);
+
+    dbg!(result);
+}
