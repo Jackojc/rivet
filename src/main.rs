29af08b73a2bb0a08acf2430225a994ab9dae39e
Jacob Sinclair <jcbsnclr@outlook.com>
2023-09-03 20:29:35 +0100

    pest parser. currently broken

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,16 @@ mod parser;
 use parser::Rule;
 use pest::Parser;
 
-fn main() {}
+fn main() {
+    let result = parser::RivetParser::parse(
+        Rule::program,
+        r#"
+        symbol a
+        "#,
+    )
+    .unwrap()
+    .next()
+    .unwrap();
+
+    dbg!(result);
+}
